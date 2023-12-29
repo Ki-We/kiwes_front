4394d22992afecd429fee21a2a99f9c082cdebd2
wodnd0131 <wodnd0131@naver.com>
2023-12-29 17:11:27 +0900

    fix:eslintrc

--- Kiwes/src/pages/Wish.tsx
+++ Kiwes/src/pages/Wish.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import UploadImageTest from './UploadImageTest';
 
 export function Wish() {
   return (
     <SafeAreaView>
       <Text>Wish Screen</Text>
+      <UploadImageTest />
     </SafeAreaView>
   );
 }
