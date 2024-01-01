08abfd7eaf490f9f8ab78a24e4a5d93e9f1ea329
wodnd0131 <wodnd0131@naver.com>
2024-01-01 22:49:00 +0900

    feat:wishCSS

--- Kiwes/src/components/BoardList.tsx
+++ Kiwes/src/components/BoardList.tsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList, Image, Text, TouchableOpacity} from 'react-native';
+import {FlatList, Image, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { RESTAPIBuilder } from '../utils/restapiBuilder';
 import { apiServer } from '../utils/metaData';
+import { View } from 'native-base';
 
 interface BoardPost {
   locationsKeyword: string;
@@ -14,7 +15,7 @@ interface BoardPost {
   languages: string[];
 }
 
-const BoardList = ({url}:any, {navigation}: any) => {
+const BoardList = ({url}:any) => {
   // const url =`${apiServer}/api/v1/heart/club_list`;
   const [posts, setPosts] = useState<BoardPost[]>([]);
   useEffect(() => {
@@ -42,23 +43,60 @@ const BoardList = ({url}:any, {navigation}: any) => {
   };
 
   return (
+
     <FlatList
       data={posts}
       keyExtractor={item => item.clubId}
       renderItem={({item}) => (
-        <TouchableOpacity onPress={() => {navigation.navigate('ClubPage', { club_id: item.clubId });}}>
-          <Image source={{uri: item.thumbnailImage}} style={{width: '100%', height: 200}} />
-          <Text>{item.title}</Text>
-          <Text>{item.date}</Text>
-          <Text>{item.locationsKeyword}</Text>
-          <Text>{item.languages}</Text>
-          <TouchableOpacity onPress={() => toggleLike(item.clubId)}>
-          <Icon name={item.heart ? 'heart' : 'heart-o'} size={25} color="#58C047" />
-          </TouchableOpacity>
+      <TouchableOpacity style={styles.clubContainer}>
+        <Image source={{uri: item.thumbnailImage}} style={styles.imageContainer} />
+
+        <View style={styles.textContainer}>
+          <View>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text>{item.date}</Text>
+            <Text>{item.locationsKeyword}</Text>
+            <Text>{item.languages}</Text>
+          </View>
+
+         
+        </View>
+        <TouchableOpacity style={styles.heartContainer} onPress={() => toggleLike(item.clubId)}>
+            <Icon name={item.heart ? 'heart' : 'heart-o'} size={25} color="#58C047" />
         </TouchableOpacity>
-      )}
-    />
+      </TouchableOpacity>
+    )}
+  />
   );
 };
-
+const styles = StyleSheet.create({
+  clubContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.2)', 
+    marginLeft: 10, 
+    marginRight: 10,
+  },
+  imageContainer: {
+    width: 120,
+    height: 100,
+    borderRadius: 30,
+  },
+  textContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    marginLeft: 10,
+  },
+  heartContainer: {
+    alignSelf: 'flex-end',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight :'bold',
+  },
+});
 export default BoardList;
--- Kiwes/src/pages/WishList.tsx
+++ Kiwes/src/pages/WishList.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import BoardList from '../components/BoardList';
 import { apiServer } from '../utils/metaData';
+import { NativeBaseProvider } from 'native-base';
 
 export function WishList({navigation}: any) {
   const url =`${apiServer}/api/v1/heart/club_list`;
   return (
-    <SafeAreaView>
-      <BoardList url={url} navigation ={navigation} />
-    </SafeAreaView>
+    <NativeBaseProvider>
+      <SafeAreaView>
+        <BoardList url={url} />
+      </SafeAreaView> 
+    </NativeBaseProvider>
   );
 }
 
