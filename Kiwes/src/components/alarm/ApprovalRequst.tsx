1c2946a8cf919bb3293c47f50f899b7bdbee809d
wodnd0131 <wodnd0131@naver.com>
2024-01-16 19:29:27 +0900

    fix:search&post

--- Kiwes/src/components/alarm/ApprovalRequst.tsx
+++ Kiwes/src/components/alarm/ApprovalRequst.tsx
@@ -7,7 +7,6 @@ import {
   TouchableOpacity,
   Image,
 } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
 import {useFocusEffect} from '@react-navigation/native';
 import {ClubApprovalRequest} from '../../utils/commonInterface';
 import {RESTAPIBuilder} from '../../utils/restapiBuilder';
